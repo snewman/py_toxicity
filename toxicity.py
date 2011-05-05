365cc5a0290cffef2968309577f091cad764b6a9
Sam Newman <sam.newman@gmail.com>
2011-05-05 11:51:17 +0100

    Printing out scores per file, need to compress them down

--- toxicity.py
+++ toxicity.py
@@ -17,13 +17,15 @@ def copy_java_tree(path, dest):
  
 def run_checkstyle(source_dir, output_file):
                 try:
-                                retcode = call('java -jar checkstyle-5.3-all.jar -f xml -c metrics.xml -o ' + output_file + ' -r ' + source_dir, shell=True)
-                                if retcode < 0:
-                                                print >>sys.stderr, "Child was terminated by signal", -retcode
-                                else:
-                                                print >>sys.stderr, "Child returned", retcode
+                    cmd = 'java -jar checkstyle-all-4.4.jar -f xml -c metrics.xml -o ' + output_file + ' -r ' + source_dir
+                    print "Running ", cmd
+                    retcode = call(cmd, shell=True)
+                    if retcode < 0:
+                        print >>sys.stderr, "Child was terminated by signal", -retcode
+                    else:
+                        print >>sys.stderr, "Child returned", retcode
                 except OSError, e:
-                                print >>sys.stderr, "Execution failed:", e
+                    print >>sys.stderr, "Execution failed:", e
  
 def toxicity_score(actual, allowed):
                 return float(actual) / float(allowed)
@@ -51,30 +53,49 @@ def score_of_violation(msg):
                                                 return (rule[0], toxicity_score(match.group(1), match.group(2)))
                                                
                 logging.warning("Unknown violation type " + msg)
-                return ("Unknown", 1.0)
- 
+                return ("Unknown", 0.0)
+
+def name_of_violation(msg):
+    for rule in CHECKSTYLE_RULES.items():
+         match = re.match(rule[1], msg)
+         if match:
+             return rule[0]
+
+    return "Unknown"
+                                    
 def main(argv):
                 timestamp = str(time.time())
-                temp_dir = 'c:/temp/glp-analysis-' + timestamp
-                checktyle_output = 'analysis' + timestamp + '.xml'
+                checkstyle_output = 'analysis' + timestamp + '.xml'
+                src_dir = argv[0]
                
                 #Needed because checkstyle 5.3 picks up non-java files when invoked from the command-line
-                #copy_java_tree('c:/GLPWorkspace/glp_workspace/ITT/mainline/CLP', temp_dir)
-                #run_checkstyle(source_dir, checktyle_output)
+                #copy_java_tree(src_dir, temp_dir)
+                run_checkstyle(src_dir, checkstyle_output)
                
-                dom = parse('analysis1290598710.22.xml')
-                violators = []
+                dom = parse(checkstyle_output)
+                violations = set()
+                file_scores = {}
+                
                 for file in dom.getElementsByTagName('file'):
-                                errors = file.getElementsByTagName('error')
+                    errors = file.getElementsByTagName('error')
                                
-                                if len(errors) > 0:
-                                                scores = {}
-                                                #Need to convert to maps to pull into a table..
-                                                scores = map(lambda error:  score_of_violation(error.getAttribute('message')), errors)
-                                                print "Scores for ", file.getAttribute("name"), scores
- 
-                for violator in violators:
-                                print(violator)
+                    if len(errors) > 0:
+                        #Need to convert to maps to pull into a table...
+                        file = file.getAttribute("name")
+                        file_scores[file] = map(lambda error:  score_of_violation(error.getAttribute('message')), errors)
+                        
+                        for error in errors:
+                            violations.add(name_of_violation(error.getAttribute('message')))
+
+                print "File name",violations
+                
+                for file, scores in file_scores.iteritems():
+                    score_line = file + "," + str(scores)
+                   # for violation in violations:
+                   #     score_line = score_line + ',' + scores[violation]
+                        
+                    print score_line
+                    #    print file, ",", scores
                
 if __name__ == "__main__":
     main(sys.argv[1:])
